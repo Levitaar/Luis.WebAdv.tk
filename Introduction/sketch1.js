3b3d38b855d03b21909b4efd6909342dd29a4ce0
Luis Huiza <huizl151@newschool.edu>
2016-09-17 18:59:00 +0000

    arrows ready

--- Introduction/sketch1.js
+++ Introduction/sketch1.js
@@ -12,6 +12,9 @@ function draw() {
   textSize(30);
   fill(255);
   text("My name is Luis Huiza", 60, 60);
+  text("My biggest hobby is gaming", 60, 180);
+  text("I enjoy games of all kinds, although videogames are my specialty", 60, 300);
+
   
   
 }
